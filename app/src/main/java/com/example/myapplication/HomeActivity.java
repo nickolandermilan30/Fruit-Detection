ddda420507e929900be0e94803e3c96c809d0ce9
Nicko <nickolander30@gmail.com>
2024-04-09 16:51:47 +0800

    April 9 2024

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,21 +1,31 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home); // Itakda ang layout
 
+        // Hanapin ang button gamit ang ID
+        Button getStartedButton = findViewById(R.id.next);
 
-
+        // Magdagdag ng OnClickListener sa button
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Lumikha ng Intent para sa NoteActivity
+                Intent intent = new Intent(HomeActivity.this, NoteActivity.class);
+                // Simulan ang NoteActivity
+                startActivity(intent);
+            }
+        });
     }
 }
